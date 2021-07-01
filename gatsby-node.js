c3d5aa545ba2b9c0e5524f75f838637477533b75
Mo <mo@motanveer.com>
2021-07-01 18:22:28 -0400

    Re-did how slugs are generated, now they are based off of the frontmatter data

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,24 +7,26 @@ exports.createPages = async ({ graphql, actions }) => {
       query {
         allMdx {
           nodes {
-            slug
+            frontmatter{
+              slug
+            }
           }
         }
       }    
     `)
       console.log(JSON.stringify(result, null, 4))
-  
-      console.log(result.data.allMdx.nodes)
-      
+
       result.data.allMdx.nodes.forEach(node => {
         console.log(node.slug)
         createPage({
-          path: "blog/" +node.slug,
+          path: "blog/" +node.frontmatter.slug,
           component: path.resolve(`./src/templates/Post.js`),
           context:{
-            slug:  "blog/" +node.slug,
+            slug:  "blog/" +node.frontmatter.slug
           }, 
         })
       })
+  
+  
   }
   
