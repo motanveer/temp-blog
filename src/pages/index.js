07fbeceee5e2beaa4e50e339d59ac332939cf8f1
Mo <mo@motanveer.com>
2021-11-25 00:26:22 -0500

    New hero image, and some breaking changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,27 @@ import SEO from "../components/seo"
 const IndexPage = () => {
   return (
     <div>
-      <SEO title="Mo Tanveer"/>
-      <Navbar/>
+
+      <SEO title="Mo Tanveer" />
+      <Navbar />
       <div className={styles.contentContainer}>
-      <StaticImage className={styles.heroImage}
-      src="../images/Mo_Tanveer-min.png"
-      alt="Mo Tanveer"
-      placeholder="tracedSVG"
-      layout="constrained"
-    />
-        <h1 className={styles.heroText}>Hi. I’m Mo Tanveer— a marketer, designer, and front-end developer.</h1>
-        <div className={styles.bio}>
-        I’m a 2000s Disney Channel aficionado. Aspiring film maker and actor. I also love skateboarding, collection action figures, and building cool things.
+        <div className={styles.heroSection}>
+        
+          <StaticImage className={styles.heroImage}
+            src="../images/Mo-Tanveer.jpg"
+            alt="Mo Tanveer"
+            placeholder="tracedSVG"
+            layout="constrained"
+          />
+
+<h1 className={styles.heroText}>Hi. I’m Mo Tanveer— Actor, Digital Creator, and Growth Marketer.</h1>
+          
         </div>
-        <Footer/>
+        <div className={styles.bio}>
+          I’m a 2000s Disney Channel aficionado that loves skateboarding and thrift shopping. <br/>Ask me about startups, marketing, design, sales, web development, filmmaking, and VFX.
+        </div>
+
+        <Footer />
       </div>
     </div>
   )
